118b39ffef1594ed15d85a7360d14cb2bfaa7bef
Zeni <gus_ghz@live.com>
2023-12-12 03:36:49 -0300

    add video test

--- test_video.py
+++ test_video.py
@@ -5,7 +5,7 @@ import os
 
 #input_file = "/home/ray/dev/PDI/aps/poring.v1i.yolov8/2023-11-28_14-32-02.mp4"
 
-input_file = "/home/ray/dev/PDI/aps/poring.v1i.yolov8/video.mp4"
-model = YOLO('/home/ray/dev/PDI/aps/poring.v1i.yolov8/runs/detect/train5/weights/best.pt')
+input_file = "D:/UTFPR/2023-2/pdi/projeto_final/yolo8PDI/video/pay_dun00.mp4"
+model = YOLO('D:/UTFPR/2023-2/pdi/projeto_final/yolo8PDI/runs/detect/train20/weights/best.pt')
 
 results = model.predict(source=input_file, save=True)
